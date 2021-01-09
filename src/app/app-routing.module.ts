512edd80f6ce93f3c6f1d34269a2a82bfcbe00e8
Carlos <carlosaguinaga01@gmail.com>
2021-01-09 01:01:44 -0500

    previo a servicios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+
 
   const routers : Routes = [
-    {path: 'heroes', component: HeroesComponent},
     {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'heroes', component: HeroesComponent}
+
 ];
 
 @NgModule({
